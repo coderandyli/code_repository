e863996c8f98cd9975c55ddb701fbec758cc8adb
coderandyli <coderandyli@163.com>
2018-12-15 22:57:16 +0800

    no message

---
+++ ai/src/test/java/com/coderandyli/ai/AiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.coderandyli.ai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
