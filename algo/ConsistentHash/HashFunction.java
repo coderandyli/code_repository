db7c44078354acd2f93bc058f5fa2f5acf724324
coderandyli <coderandyli@163.com>
2019-07-08 22:51:15 +0800

    一致性hash算法

---
+++ algo/ConsistentHash/HashFunction.java
@@ -0,0 +1,30 @@
+package ConsistentHash;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by lizhen on 2019-07-08
+ */
+public class HashFunction {
+    private MessageDigest md5 = null;
+
+    public long hash(String key) {
+        if (md5 == null) {
+            try {
+                md5 = MessageDigest.getInstance("MD5");
+            } catch (NoSuchAlgorithmException e) {
+                throw new IllegalStateException("no md5 algrithm found");
+            }
+        }
+
+        md5.reset();
+        md5.update(key.getBytes());
+        byte[] bKey = md5.digest();
+        //具体的哈希函数实现细节--每个字节 & 0xFF 再移位
+        long result = ((long) (bKey[3] & 0xFF) << 24)
+                | ((long) (bKey[2] & 0xFF) << 16
+                | ((long) (bKey[1] & 0xFF) << 8) | (long) (bKey[0] & 0xFF));
+        return result & 0xffffffffL;
+    }
+}
