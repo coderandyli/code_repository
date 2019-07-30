e70ea22c7b5882872a0e099b3970f0abbe9ad6e1
coderandyli <coderandyli@163.com>
2019-07-30 11:27:28 +0800

    project-demo init

---
+++ project-demo/src/main/java/com/coderandyli/project/ProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.coderandyli.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDemoApplication.class, args);
+    }
+
+}
