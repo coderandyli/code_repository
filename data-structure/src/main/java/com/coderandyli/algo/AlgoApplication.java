668336b2a837279fb85e118cd24764fe85fe7c53
coderandyli <coderandyli@163.com>
2019-08-02 10:49:14 +0800

    data-structure init

---
+++ data-structure/src/main/java/com/coderandyli/algo/AlgoApplication.java
@@ -0,0 +1,13 @@
+package com.coderandyli.algo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoApplication.class, args);
+    }
+
+}
