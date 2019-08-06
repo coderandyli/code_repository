1de68f828f9183fce85c6e6f2240ca1655408124
coderandyli <coderandyli@163.com>
2019-08-06 13:21:01 +0800

    1.基于int[]实现的BitMap 2.HyperLogLog算法测试

---
+++ data-structure/src/main/java/com/coderandyli/algo/BitMap/BitMap.java
@@ -0,0 +1,85 @@
+package com.coderandyli.algo.BitMap;
+
+/**
+ * Created by lizhen on 2019-08-05
+ * 基于int[] 实现的BitMap
+ *
+ *
+ * 0x01
+ *   - 0x 表示16进制
+ *
+ * 问题
+ *  1、涉及到位运算为什么会使用16进制呢?
+ *  https://blog.csdn.net/zhongdong00/article/details/83049053
+ *
+ */
+public class BitMap {
+
+    private final static int ADDRESS_BITS_PER_WORD = 5;  // 5
+    private final static int BITS_PER_WORD = 1 << ADDRESS_BITS_PER_WORD; //  32
+    private final static int BIT_INDEX_MASK = BITS_PER_WORD - 1; // 31
+
+
+    private static int getWordIndex(int bitIndex) {
+        return bitIndex >> ADDRESS_BITS_PER_WORD;
+    }
+
+    private int length;
+    public int[] words;
+
+    public BitMap(int length) {
+        if (length < 0) {
+            System.out.println("长度不合法");
+            return;
+        }
+
+        this.length = length;
+        this.words = new int[(getWordIndex(length)) + (length & BIT_INDEX_MASK) > 0 ? 1 : 0];
+    }
+
+    /**
+     * @param bitIndex
+     * @param value
+     */
+    public void set(int bitIndex, boolean value) {
+        if (value) {
+            // 获取wordIndex
+            int wordIndex = getWordIndex(bitIndex);
+            int word = words[wordIndex];
+            // 获取在word中偏移量
+            int offset = bitIndex & BIT_INDEX_MASK;
+            // 赋值
+            words[wordIndex] = word | (0x01 << offset);
+        } else {
+            // todo 未实现
+        }
+    }
+
+    public int getBit(int bitIndex) {
+        int wordIndex = getWordIndex(bitIndex);
+        int word = words[wordIndex];
+        // 获取在word中偏移量
+        int offset = bitIndex & BIT_INDEX_MASK;
+        return word >> offset & 0x01;
+    }
+
+    public static void main(String args[]) {
+
+        BitMap bitMap = new BitMap(100);
+        bitMap.set(1, true);
+        bitMap.set(2, true);
+        bitMap.set(3, true);
+
+        System.out.println(bitMap.getBit(1));
+        System.out.println(bitMap.getBit(2));
+        System.out.println(bitMap.getBit(3));
+        System.out.println(bitMap.getBit(4));
+
+        int[] words = bitMap.words;
+        for (int word : words) {
+            System.out.println(word);
+        }
+    }
+
+
+}
--- data-structure/src/main/java/com/coderandyli/algo/BitMap/BitSetTest.java
+++ data-structure/src/main/java/com/coderandyli/algo/BitMap/BitSetTest.java
@@ -2,31 +2,31 @@ package com.coderandyli.algo.BitMap;
 
 import sun.jvm.hotspot.debugger.Address;
 
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 
 
 /**
  * Created by lizhen on 2019-08-02
  */
-
-public class Main {
+public class BitSetTest {
 
     public static void main(String[] args) {
         test01();
     }
 
-
     /**
      * 数组的排序去重
      * BitSet 底层是一个long类型的数组
      */
-    public static void test01(){
-        int [] array = new int [] {1,2,3,22,0,3,64};
-        BitSet bitSet  = new BitSet(64 * 1);
+    public static void test01() {
+        int[] array = new int[]{1, 2, 3, 22, 0, 3, 64};
+        BitSet bitSet = new BitSet(64 * 1);
         System.out.println(bitSet.size());   //64
 
         //将数组内容组bitmap
-        for(int i=0;i<array.length;i++) {
+        for (int i = 0; i < array.length; i++) {
             bitSet.set(array[i], true);
         }
 
@@ -34,10 +34,15 @@ public class Main {
         System.out.println(bitSet.get(60));
 
         System.out.println("下面开始遍历BitSet：");
-        for ( int i = 0; i < bitSet.size(); i++ ){
-            System.out.println(bitSet.get(i));
+        List<Integer> results = new ArrayList<>();
+        for (int i = 0; i < bitSet.size(); i++) {
+            boolean b = bitSet.get(i);
+            if (b) {
+                results.add(i);
+            }
         }
 
+        results.stream().forEach(value -> System.out.println(value));
     }
 
 }
