30182794b95b4465c6c0c7ed69ee9ce3a5f81aeb
coderandyli <coderandyli@163.com>
2019-10-21 13:17:37 +0800

    DeepJVM

---
+++ monitor_tuning/src/main/java/com/coderandyli/monitor_tuning/DeepJVM/Test01.java
@@ -0,0 +1,15 @@
+package com.coderandyli.monitor_tuning.DeepJVM;
+
+/**
+ * Created by lizhen on 2019-10-21
+ * 6.3图6-2
+ * javac 编译查看16进制
+ */
+public class Test01 {
+
+    private int m;
+
+    public int inc(){
+        return m + 1;
+    }
+}
