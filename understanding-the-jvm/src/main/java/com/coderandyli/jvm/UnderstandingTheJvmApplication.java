039000c439a5a2d9e8d4cdc4358911aeb3652de2
coderandyli <coderandyli@163.com>
2019-11-10 21:29:45 +0800

    understanding the JVM init

---
+++ understanding-the-jvm/src/main/java/com/coderandyli/jvm/UnderstandingTheJvmApplication.java
@@ -0,0 +1,13 @@
+package com.coderandyli.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandingTheJvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderstandingTheJvmApplication.class, args);
+    }
+
+}
---
+++ understanding-the-jvm/src/test/java/com/coderandyli/jvm/UnderstandingTheJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderandyli.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandingTheJvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
