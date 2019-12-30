43bdcb8c9ab3dcea3b7511f53f0964d15522e025
coderandyli <coderandyli@163.com>
2019-12-30 20:09:19 +0800

    FastDFS Simple Demo

---
+++ fastdfsdemo/src/main/java/com/coderandyli/fastdfsdemo/FastdfsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.coderandyli.fastdfsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastdfsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastdfsdemoApplication.class, args);
+    }
+
+}
---
+++ fastdfsdemo/src/test/java/com/coderandyli/fastdfsdemo/FastdfsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderandyli.fastdfsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastdfsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
