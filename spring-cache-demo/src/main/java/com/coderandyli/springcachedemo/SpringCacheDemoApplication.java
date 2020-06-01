6451a79502f51600e5630df5ea5220131aa172ec
coderandyli <coderandyli@163.com>
2020-06-01 16:10:42 +0800

    spring cache demo init

---
+++ spring-cache-demo/src/main/java/com/coderandyli/springcachedemo/SpringCacheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.coderandyli.springcachedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheDemoApplication.class, args);
+	}
+
+}
